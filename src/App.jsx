450c8d8ffd0f9867b3be66e735de01a9cee05a24
Fahim <fahimarefin57@gmail.com>
2023-07-30 09:43:27 +0600

    React-Router installed and defined routes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+//createBrowserRouter should be created  outside component
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+  //we will not have '*' route to catch no match like before. we will do it other way
+]);
+
 function App() {
-  return <div className=" bg-red-300">App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,22 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+
+/*
+
+## install vite with react and javascript
+## config eslint
+## tailwind download done 
+
+*** React Router latest version (6.4+) ***
+-----------------------------------------------
+## we can now data loading with React Router (but React Query is recommneded)
+## previously we saw react-router with world wise app where we define router in a old way
+## here we will define router in a new way (each route is a obj). bcz now with this 6.4+ version 
+   we can load data using React Router.
+## "npm i react-router-dom@6" --> install it 
+## createBrowserRouter() --> we now create route in this func call check doc.must be created outside component
+    then use it in the jsx
+## 
+
+*/
